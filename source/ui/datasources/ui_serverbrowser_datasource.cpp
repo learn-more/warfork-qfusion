c5405a3f92aee94c3144c03ccdcb5c600766291b
Jannik Kolodziej <github@jko.dev>
2020-04-28 00:53:50 +0200

    Fixes a multiline serverbrowser issue
    Server containing only spaces as hostname were shown taking up
    two lines in the server browser. This commit works around the issue
    by replacing spaces with non-breaking spaces.

--- source/ui/datasources/ui_serverbrowser_datasource.cpp
+++ source/ui/datasources/ui_serverbrowser_datasource.cpp
@@ -301,6 +301,11 @@ void ServerInfo::fixString( std::string &s )
 		s.erase( pos, 1 );
 		pos = s.find( '\n', pos );
 	}
+	pos = s.find( ' ' );
+	while( pos != std::string::npos ) {
+		s.replace( pos, 1, "&nbsp;" );
+		pos = s.find(' ' );
+	}
 }
 
 // html encode all string fields, fix color tags and

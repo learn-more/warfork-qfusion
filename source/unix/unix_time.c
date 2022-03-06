db10c49495d30558962363a215ef09ded35607e7
ko1N <ko1N1337@gmail.com>
2022-03-07 00:40:25 +0100

    Reverted changes in unix_time.c as to mike's suggestion

--- source/unix/unix_time.c
+++ source/unix/unix_time.c
@@ -7,31 +7,18 @@
 static unsigned long sys_secbase;
 uint64_t Sys_Microseconds( void )
 {
-	static time_t sys_secbase;
-
-#ifdef HAVE_CLOCKGETTIME
-	struct timespec ts;
-	clock_gettime( CLOCK_MONOTONIC, &ts );
-
-	if( !sys_secbase ) {
-		sys_secbase = ts.tv_sec;
-		return ts.tv_nsec / 1000;
-	}
-
-	// TODO handle the wrap
-	return (uint64_t)( ts.tv_sec - sys_secbase ) * 1000000 + ts.ts_nsec / 1000;
-#else
 	struct timeval tp;
+
 	gettimeofday( &tp, NULL );
 
-	if( !sys_secbase ) {
+	if( !sys_secbase )
+	{
 		sys_secbase = tp.tv_sec;
 		return tp.tv_usec;
 	}
 
 	// TODO handle the wrap
-	return (uint64_t)( tp.tv_sec - sys_secbase ) * 1000000 + tp.tv_usec;
-#endif
+	return (uint64_t)( tp.tv_sec - sys_secbase )*1000000 + tp.tv_usec;
 }
 
 /*
